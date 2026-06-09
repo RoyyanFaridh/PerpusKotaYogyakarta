28debc56032a1498be2cca1532d4fa62f7282959
achmadidoo <arsandhy9@gmail.com>
2026-06-09 11:37:56 +0700

    hapus gtmerix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'superadmin'     => \App\Http\Middleware\IsSuperAdmin::class,
             'has.permission' => \App\Http\Middleware\HasPermission::class,
-            'gtmetrix'       => \App\Http\Middleware\GtmetrixBypass::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
